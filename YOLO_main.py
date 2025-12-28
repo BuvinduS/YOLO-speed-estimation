9befde966699105ffecbf5bf234993bd718f31de
Buvindu Suraweera <buvindudsuraweera@gmail.com>
2025-12-28 11:08:16 +0530

    Instantaneous speed removed + lines named

--- YOLO_main.py
+++ YOLO_main.py
@@ -4,15 +4,16 @@ import cv2
 import numpy as np
 
 # For Videos
-cap = cv2.VideoCapture("Videos/cars1.mp4")
-
+cap = cv2.VideoCapture("Videos/footage.mp4")
+# Dimensions for resizing
 max_width = 900
 max_height = 900
 
 if not cap.isOpened():
     print("Video Error")
 
-model = YOLO("YOLO-weights/yolov8m.pt")
+# Chose the model YOLO-weight needed, if not already available will be downloaded to the path
+model = YOLO("YOLO-weights/yolov8n.pt")
 
 fps = cap.get(cv2.CAP_PROP_FPS)
 
@@ -23,7 +24,7 @@ CAR_AVG_WIDTH = 1.75
 line1_times = {}
 line2_times = {}
 speed_by_id = {}
-LINE_DISTANCE_IN_METERS = 10
+LINE_DISTANCE_IN_METERS = 20
 
 width_samples = []
 meters_per_pixel = None
@@ -53,6 +54,7 @@ while True:
 
     img = cv2.resize(img, (width_new, height_new))
 
+    # Counting line
     line_y = int(img.shape[0] * 3 / 4)
     cv2.line(img, (0, line_y), (img.shape[1], line_y), (0, 255, 0), 2)
 
@@ -80,10 +82,10 @@ while True:
 
             track_id = int(box.id[0])
 
-            # Scale calibration near the counting line
+            # Scale calibration near the counting line (for instantaneous speed estimation only)
             if track_id not in speed_by_id:
                 if meters_per_pixel is None:
-                    if 60 < w < 200 and abs(cY - line_y) < 20:
+                    if 60 < w < 200 and abs(cY - line_y) < 10:
                         width_samples.append(w)
 
                     if len(width_samples) >= 20:
@@ -132,9 +134,6 @@ while True:
 
             prev_centers[track_id] = cY
 
-
-
-
             # Counting
             if track_id not in counted:     # not yet counted
                 counted[track_id] = False
@@ -151,7 +150,10 @@ while True:
                 instantaneous_val_colorR = (0, 255, 0)
                 cvzone.putTextRect(img, f'{speed_by_id[track_id]} kmph', (max(0, x1), max(35, y1)), scale=1, thickness=1, offset=10, colorR = (0,0,0), colorT=(0,0,255))
 
-            cvzone.putTextRect(img, f'{speed_kmph:.1f}', (max(0, x2), max(35, y2)), scale=1, thickness=1, offset=10, colorR=instantaneous_val_colorR, colorT=instantaneous_val_colorT)
+            # Instantaneous speed
+            #cvzone.putTextRect(img, f'{speed_kmph:.1f}', (max(0, x2), max(35, y2)), scale=1, thickness=1, offset=10, colorR=instantaneous_val_colorR, colorT=instantaneous_val_colorT)
+            # ID on the bottom right of the bounding box
+            cvzone.putTextRect(img, f'Id : {track_id}', (max(0, x2), max(35, y2)), scale=1, thickness=1, offset=10, colorR=instantaneous_val_colorR, colorT=instantaneous_val_colorT)
             cv2.circle(img, (cX, cY), 5, (123, 255, 24), cv2.FILLED)
 
         if meters_per_pixel is None:
@@ -160,7 +162,16 @@ while True:
             mmp = f'{meters_per_pixel:.3f}'
 
         cv2.putText(img, f'Count: {count}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
-        cv2.putText(img, f'METERS_PER_PIXEL: {mmp}', (int(img.shape[1]/4), 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+        # cv2.putText(img, f'METERS_PER_PIXEL: {mmp}', (int(img.shape[1]/4), 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+
+        # Text below line starting and ending measurement
+        cv2.putText(img, 'Start ', (10, line_start_y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        cv2.putText(img, 'End ', (10, line_end_y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+        cv2.putText(img, 'Counting Line ', (10, line_y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+        # cv2.putText(img, "Red: Start | Blue: End | Green: Count",
+        #             (10, img.shape[0] - 20),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
